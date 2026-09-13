342a12ddaf08241209980d97d037b1fcf6f2ef78
GROSAPACKS\zonce <zonceya@gmail.com>
2026-09-13 17:18:49 +0200

    url for staging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "za.co.skoolswap"
         minSdk = 25
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
