75a31ad60600702d735b8a96a2118f1eb9c7de35
GROSAPACKS\zonce <zonceya@gmail.com>
2026-09-14 19:36:36 +0200

    version 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "za.co.skoolswap"
         minSdk = 25
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
